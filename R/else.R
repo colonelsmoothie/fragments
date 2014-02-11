b9964fad3236f7dfacbc3af58eb35aa9bdcd1e8e
colonelsmoothie <colonelsmoothie@gmail.com>
2014-02-10 23:13:02 -0600

    inserted remark about else syntax

--- R/else.R
+++ R/else.R
@@ -1,4 +1,5 @@
 # This might seem somewhat silly but I just learned about else in R...kind of ridiculous since I hadn't used it until now
+# Make sure the else keyword is on the same line as the closing brace } of the if statement.
 
 i <- 2
 if(i == 1){
